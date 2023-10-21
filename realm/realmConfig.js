1ee087d7908161adacc896b63384ef17e1622cd2
mathewGlenn <glennmathewgarma1999@gmail.com>
2023-10-21 12:55:03 +0800

    fix realm

--- realm/realmConfig.js
+++ realm/realmConfig.js
@@ -1,18 +1,20 @@
 /* eslint-disable prettier/prettier */
 import Realm from 'realm';
 
-class Student {}
+class Student extends Realm.Object {}
 
 Student.schema = {
   name: 'Student',
   properties: {
+    recordID: 'string',
     name: 'string',
     address: 'string',
     course: 'string',
     yearLevel: 'string',
   },
+  primaryKey: 'recordID',
 };
 
-const realm = new Realm({ schema: [Student] });
+const realm = new Realm({ schema: [Student], schemaVersion: 6 });
 
 export default realm;
--- screens/LessonDatabaseScreen.jsx
+++ screens/LessonDatabaseScreen.jsx
@@ -5,30 +5,46 @@ import realm from '../realm/realmConfig';
 
 function LessonDatabaseScreen() {
   const [students, setStudents] = useState([]);
-  const [newStudent, setNewStudent] = useState({ name: '', address: '', course: '', yearLevel: 0 });
+  const [newStudent, setNewStudent] = useState({ recordID: '', name: '', address: '', course: '', yearLevel: '' });
   const [selectedStudent, setSelectedStudent] = useState(null);
 
+  function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+
   useEffect(() => {
-    const students = realm.objects('Student');
-    setStudents(students);
+    getStudents();
   }, []);
 
+  const getStudents = () => {
+    const students = realm.objects('Student');
+    setStudents(students);
+  }
   const handleAddStudent = () => {
     realm.write(() => {
-      realm.create('Student', newStudent);
+      realm.create('Student', newStudent);r
     });
-    setNewStudent({ name: '', address: '', course: '', yearLevel: 0 });
+    setNewStudent({ recordID: makeid(10), name: '', address: '', course: '', yearLevel: '' });
   };
 
   const handleUpdateStudent = () => {
     realm.write(() => {
-      const studentToUpdate = realm.objects('Student').filtered(`name = "${selectedStudent.name}"`)[0];
+      const studentToUpdate = realm.objects('Student').filtered(`recordID = "${selectedStudent.recordID}"`)[0];
       studentToUpdate.name = newStudent.name;
       studentToUpdate.address = newStudent.address;
       studentToUpdate.course = newStudent.course;
       studentToUpdate.yearLevel = newStudent.yearLevel;
     });
-    setNewStudent({ name: '', address: '', course: '', yearLevel: 0 });
+    setNewStudent({ recordID: '', name: '', address: '', course: '', yearLevel: '' });
     setSelectedStudent(null);
   };
 
@@ -36,11 +52,13 @@ function LessonDatabaseScreen() {
     realm.write(() => {
       realm.delete(student);
     });
+    getStudents();
   };
 
   const handleSelectStudent = (student) => {
     setSelectedStudent(student);
     setNewStudent({
+      recordID: student.recordID,
       name: student.name,
       address: student.address,
       course: student.course,
@@ -69,7 +87,7 @@ function LessonDatabaseScreen() {
       <TextInput
         placeholder="Year Level"
         value={newStudent.yearLevel.toString()}
-        onChangeText={(text) => setNewStudent({ ...newStudent, yearLevel: parseInt(text) })}
+        onChangeText={(text) => setNewStudent({ ...newStudent, yearLevel: text })}
       />
       {selectedStudent ? (
         <Button title="Update" onPress={handleUpdateStudent} />
@@ -81,12 +99,13 @@ function LessonDatabaseScreen() {
         data={students}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <View>
+          <View style={{padding: 20}}>
             <Text>{item.name}</Text>
             <Text>{item.address}</Text>
             <Text>{item.course}</Text>
             <Text>{item.yearLevel}</Text>
             <Button title="Edit" onPress={() => handleSelectStudent(item)} />
+            <View style={{marginTop: 10}}  />
             <Button title="Delete" onPress={() => handleDeleteStudent(item)} />
           </View>
         )}
